307108c8356da90d46c3e29dc9945678fe3984f8
Talal <talalothmanse@gmail.com>
2023-03-21 07:24:19 +0800

    -Changed the footer content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="footer footer-center p-4 bg-base-300 text-base-content">
   <div>
-    <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+    <p>Crafted with ❤️ by Talal Al-Ahdal</p>
   </div>
 </footer>
 </template>
