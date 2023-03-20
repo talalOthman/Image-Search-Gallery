59531b7121d632b11a766876de771a883829bdca
Talal <talalothmanse@gmail.com>
2023-03-21 04:56:55 +0800

    -Minor refactor on 'Body' component

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="border flex flex-col grow w-screen p-2 items-center gap-y-3">
+    <div class=" flex flex-col grow w-screen p-2 items-center gap-y-3">
         <input type="text" placeholder="Enter image topic" class="input input-bordered input-primary w-full max-w-xs" />
-        <div class="border flex grow ">
+        <div class=" flex flex-col grow justify-center">
             <p>Your images will display here</p>
         </div>
     </div>
