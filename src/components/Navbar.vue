b4c6ad6108a60fffaabe7485b1f5289aa9b05144
Talal <talalothmanse@gmail.com>
2023-03-21 18:33:07 +0800

    -Made the navbar sticky

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="navbar bg-primary text-primary-content">
+    <div class="navbar bg-primary text-primary-content sticky top-0 z-40 opacity-90">
         <a class="btn btn-ghost normal-case text-xl">Image Search Gallery</a>
     </div>
 </template>
