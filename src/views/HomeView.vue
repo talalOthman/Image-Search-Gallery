332c29d684593de8721908825cc92c0872ebe7de
Talal <talalothmanse@gmail.com>
2023-03-31 02:46:53 +0800

    -Fixed light mode issue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@ import { RouterLink } from 'vue-router'
         <div
             class="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
             <h1 class="md:text-6xl text-4xl font-bold leading-none "> Discover
-                <span class="dark:text-primary">Stunning</span> Images
+                <span class="text-primary">Stunning</span> Images
             </h1>
             <p class="px-8 mt-8 mb-12 text-md md:text-lg">Browse through our vast collection of images and find what you're
                 looking for in just a few clicks.</p>
